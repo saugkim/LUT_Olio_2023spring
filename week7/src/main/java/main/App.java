19c593a78ee67379a1bd29ca4711d99160da81f7
Kim <saugkim@users.noreply.github.com>
2023-02-27 22:00:06 +0200

    week 7 pre

---
+++ week7/src/main/java/main/App.java
@@ -0,0 +1,10 @@
+package main;
+
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        
+		System.out.println("Hello");
+    }
+}
---
+++ week7/src/main/java/main/Wordle.java
@@ -0,0 +1,21 @@
+package main;
+
+
+
+public class Wordle {
+    
+    private final static String FILENAME = "words.txt";
+    private int attempt;
+    
+
+    public Wordle() {
+        attempt = 1;
+        readFile();
+
+    }
+
+    private void readFile() {
+
+        
+    }
+}
