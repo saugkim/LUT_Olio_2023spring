37d1953b3606e356cf3331dd763d04e258faf217
Kim <saugkim@users.noreply.github.com>
2023-02-28 10:28:21 +0200

    update temp

--- week7/src/main/java/main/App.java
+++ week7/src/main/java/main/App.java
@@ -3,8 +3,9 @@ package main;
 import java.util.Scanner;
 
 public class App {
-    public static void main(String[] args) {
-        
-		System.out.println("Hello");
+
+    private final static String YELLOW = "\u001B[33m";
+    public static void main(String[] args) { 
+  	  	System.out.println("\u001B[33m-XbXXX");
     }
 }
